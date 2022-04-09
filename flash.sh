17e2e84cb6dbf6ac586e2eb80f0a35b8bd1f0fd9
lurk101 <lurkone0one@gmail.com>
2022-04-09 19:01:42 -0400

    update comm-lib

--- flash.sh
+++ flash.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for pico in {0..3}
 do
-openocd -f /home/pi/configs/raspberrypi-swd-$pico.cfg -f target/rp2040.cfg -c "program $1 reset exit"
+    echo "========= FLASH $pico"
+    ../resetall.sh
+    openocd -f /home/pi/configs/raspberrypi-swd-$pico.cfg -f target/rp2040.cfg -c "program $1 reset exit"
+    if [ $? -ne 0 ] ; then
+        exit
+    fi
 done
